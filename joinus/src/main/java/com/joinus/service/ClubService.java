2c3548e2dfad01eca1fd9ea4bb5b59028a4b8130
SubinHeo0 <anmedi3623@naver.com>
2022-07-09 19:24:05 +0900

    [fix] 충돌

--- joinus/src/main/java/com/joinus/service/ClubService.java
+++ joinus/src/main/java/com/joinus/service/ClubService.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.joinus.domain.ClubMembersVo;
-import com.joinus.domain.ClubTotalBean;
-import com.joinus.domain.BoardCriteria;
 import com.joinus.domain.BoardTotalBean;
 import com.joinus.domain.ClubBoardsVo;
 
